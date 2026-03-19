051e120227ca2c41274520605a7ae003b8bfb90c
SayanovVG <v.g.sayanov@gmail.com>
2026-03-19 17:24:04 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mt-v15';
+const CACHE = 'mt-v16';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
